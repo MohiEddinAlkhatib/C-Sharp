17da27962c2bc918346e91526f2e6ed23f8ea260
MohiEddinAlkhatib <MohiEddin.AlKhatib@outlook.com>
2026-09-17 10:19:29 +0300

    Update Program.cs

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -34,8 +34,8 @@ class Program
         Employee emp = new Employee
         {
             Id = 202,
-            FirstName = "Kawthar",
-            LastName = "Rivera"
+            FirstName = "MohiEddin",
+            LastName = "AlKhatib"
         };
 
         // 🎭 POLYMORPHISM MAGIC:
